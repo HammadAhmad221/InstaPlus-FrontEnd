3933b23b0dd19c081a5a15c266ec785e1c707d34
Hammad <hammadchahal123@gmail.com>
2023-09-07 11:44:32 +0500

    only responcivness is left..

---
+++ src/app/tabels/tabels.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabels',
+  templateUrl: './tabels.component.html',
+  styleUrls: ['./tabels.component.scss']
+})
+export class TabelsComponent {
+
+}
